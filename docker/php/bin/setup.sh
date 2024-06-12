de9fe7e1be6d24778e15383aa33043677a928a4c
Holger Veltrup <veltrup@sitepark.com>
2024-06-12 15:18:02 +0200

    fix: worker check

--- docker/php/bin/setup.sh
+++ docker/php/bin/setup.sh
@@ -6,4 +6,4 @@ su www-data -s /bin/bash -c "/tools/install-app.sh atoolo-e2e-test"
 ln -s /apps/atoolo-e2e-test /var/www/atoolo-e2e-test/www/app
 su www-data -s /bin/bash -c "/var/www/atoolo-e2e-test/www/app/bin/console search:indexer"
 # run runtime-check-scheduler once
-su www-data -s /bin/bash -c "/var/www/atoolo-e2e-test/www/app/bin/console messenger:consume --all --limit 1"
+su www-data -s /bin/bash -c "/var/www/atoolo-e2e-test/www/app/bin/console messenger:consume --limit 1 scheduler_atoolo-runtime-check"
