a609e9ba09e3dfbce67d82eb5aa6f66b1253d354
Holger Veltrup <veltrup@sitepark.com>
2024-07-02 12:20:37 +0200

    feat: switch to php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new \PhpCsFixer\Finder())
+    ->in(__DIR__ . '/test');
+
+return (new \PhpCsFixer\Config())
+    ->setCacheFile('var/cache/php-cs-fixer')
+    ->setFinder($finder)
+    ->setRules(['@PER-CS' => true]);
