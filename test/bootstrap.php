dcf8f99a9db5bf94c0b215d841c3b42bb5e42b24
Holger Veltrup <veltrup@sitepark.com>
2025-01-30 10:21:37 +0100

    test: fix form-api test

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 include __DIR__ . '/../vendor/autoload.php';
 
 $_SERVER['ENDPOINT_BASE'] = $_SERVER['ENDPOINT_BASE'] ?? 'http://atoolo-e2e-test:9090';
-$_SERVER['MAILPIT_ENDPOINT_BASE'] = $_SERVER['MAILPIT_ENDPOINT_BASE'] ?? 'http://atoolo-e2e-test:9090';
+$_SERVER['MAILPIT_ENDPOINT_BASE'] = $_SERVER['MAILPIT_ENDPOINT_BASE'] ?? 'http://atoolo-e2e-test:8025';
 
 echo "ENDPOINT_BASE: " . $_SERVER['ENDPOINT_BASE'] . "\n";
 echo "MAILPIT_ENDPOINT_BASE: " . $_SERVER['MAILPIT_ENDPOINT_BASE'] . "\n";
